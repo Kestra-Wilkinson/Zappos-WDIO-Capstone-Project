f9bdcacb80824283d857cc30f002cebc9f399740
Kestra-Wilkinson <kestra.wilkinson6530@stu.mtec.edu>
2025-05-12 09:36:46 -0600

    Update search.test.js

--- test/specs/search.test.js
+++ test/specs/search.test.js
@@ -1,12 +1,7 @@
 import SearchPage from '../pageobjects/SearchPage';
 
 describe('Zappos Search Tests', () => {
-    const tests =[
-        {term: 'Sports',valid:true},
-        {term: 'Sunglasses',valid:true},
-        {term: 'Mules',valid: true},
-        {term: 'Hiking', valid:true}
-    ];
+   
 
          tests.forEach(({term, valid }) => {
             it(`should ${valid ? 'return':'not return'} results for "${term}`,async () =>{
