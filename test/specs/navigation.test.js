30996d2db89c92716bfe67d9291486f2a3cb6db5
Kestra <kestra.wilkinson6530@stu.mtec.edu>
2025-05-11 19:24:30 -0600

    changes

--- test/specs/navigation.test.js
+++ test/specs/navigation.test.js
@@ -1,13 +1,5 @@
 import NavigationPage from '../pageobjects/NavigationPage.js';
 
-const itemstoTest =[
-"All Men's Shoes", "Sneakers & Athletic", "Boots", "Loafers" , "Oxfords", "Sandals",
-"Slippers", "Hiking","Work & Safety Sneakers","Work & Duty Boots", "Adaptive", "Wide",
-"All Men's Clothing", "Shirts & Tops", "Pants", "Coats & OuterWear", "Jeans",
-"Hoodies & SweatShirts", "Shorts", "Big & Tall", "ActiveWear", "Work & Duty Apparel", "Sleepwear",
-"All Men's Accessories", "Hats", "Bags","Belts","Watches","Sunglasses & Eyewear", 
-"Wallets","Gloves", "adidas", "Birkenstock","Brooks","Converse", "Hey Dude", "Merrell" , "The North Face"
-];
 
 const itemsandTerms = [
 {menuItem: "All Men's Shoes", resultName: "Men's Shoes"},
