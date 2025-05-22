6109b69a1c65a11578391a1ed5dfecfad4490872
Kestra Wilkinson <kestra.wilkinson6530@stu.mtec.edu>
2025-05-22 01:22:21 -0700

    added Women's Nav Test

--- test/specs/navigation.test.js
+++ test/specs/navigation.test.js
@@ -1,4 +1,5 @@
 import NavigationPage from '../pageobjects/NavigationPage.js';
+import WomenSNavigation from '../pageobjects/Women\'s Navigation.js';
 
 
 
@@ -16,8 +17,17 @@ describe('Zappos Men Menu Navigation',() => {
 
  
  
+
  
  
  
  
- 
+ describe('Zappos Women Menu Navigation',() => {
+          WomenSNavigation.itemsandTerms.forEach(({menuItem, resultName}) => {
+          it(`should navigate to: ${menuItem}`, async () => { 
+            await WomenSNavigation.openWomenMenu();
+            await WomenSNavigation.clickMenuOptionByText(menuItem,resultName);
+            await WomenSNavigation.validateNavigation(resultName);
+          });
+     });  
+ });
