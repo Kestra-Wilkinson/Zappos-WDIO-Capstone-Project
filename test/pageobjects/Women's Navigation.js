447089897da0d55f37b5518734cc01aa48bf5711
Kestra <kestra.wilkinson6530@stu.mtec.edu>
2025-06-04 14:52:10 -0600

    fixed Navigation Order

--- test/pageobjects/Women's Navigation.js
+++ test/pageobjects/Women's Navigation.js
@@ -12,33 +12,35 @@ get resultcount(){return $('.yu-z');}
 itemsandTerms = [
 {menuItem: "All Women's Shoes",resultName:" Women's Shoes"},
 {menuItem:"Sneakers & Athletic",resultName:"Women's Sneakers & Athletic Shoes"},
-{menuItem:"Boots & Booties",resultName: "Womens Boots"},
+{menuItem:"Sandals",resultName: "Womens Sandals"},
 {menuItem:"Clogs",resultName:"Women's Clogs"},
 {menuItem:"Flats",resultName: "Women's Flats"},
-{menuItem:"Slippers",resultName: "Women's Slippers"},
+{menuItem:"Heels",resultName:"Women's Heels"},
 {menuItem:"Mules",resultName: "Women's Mules Shoes"},
 {menuItem:"Loafers",resultName:"Women's Loafers"},
-{menuItem:"Sandals",resultName: "Womens Sandals"},
+{menuItem:"Boots & Booties",resultName:"Womens Boots"},
 {menuItem:"Cowboy Boots",resultName:"Women's Western Cowboy Boots"},
 {menuItem:"Composite Toe",resultName:"Composite Toe"},
 {menuItem:"Safety Shoes",resultName: "Safety Shoes"},
-{menuItem:"Adaptive",resultName:"Women's Adaptive Shoes"},
-{menuItem:"Heels",resultName: "Women's Heels"}, 
-{menuItem: "All Women's Clothing",resultName:"Womens Clothing"},
-{menuItem: "Dresses",resultName:  "Women's Dresses"},
-{menuItem: "Shirts & Tops",resultName:"Women's Shirts & Tops"},
-{menuItem: "Jeans & Denim",resultName: "Women's Jeans"},
-{menuItem: "Jumpsuits & Rompers",resultName:"Women's Jumpsuits & Rompers"},
-{menuItem:  "Shorts",resultName:"Women's Shorts"},
-{menuItem: "Skirts",resultName:"Women's Skirts"},
-{menuItem: "Swimwear & Cover Ups",resultName: "Women's Swimwear"},
-{menuItem: "Pants",resultName: "Women's Pants"},
-{menuItem: "Activewear",resultName: "Women's Athletic Clothing"},
-{menuItem:  "Sleepwear", resultName: "Women's Sleepwear"},
-{menuItem: "Coats & Outerwear",resultName:  "Women's Coats & Outerwear"},
-{menuItem:  "Plus Size",resultName: "Women's Plus Size Clothing"},
-{menuItem:  "Work & Duty Apparel",resultName:"Women's Work & Duty Clothing"},
-{menuItem:  "All Women's Accessories",resultName: "Women's Accessories"},
+{menuItem:"Work Shoes",resultName: "Work Shoes"},
+{menuItem:"Adaptive",resultName:"Women's Adaptive Shoes"}, 
+{menuItem:"Comfort",resultName:"Women's Comfort Shoes"},
+{menuItem:"Slippers",resultName:"Women's Slippers"},
+{menuItem:"All Women's Clothing",resultName:"Womens Clothing"},
+{menuItem:"Dresses",resultName:  "Women's Dresses"},
+{menuItem:"Shirts & Tops",resultName:"Women's Shirts & Tops"},
+{menuItem:"Jeans & Denim",resultName: "Women's Jeans"},
+{menuItem:"Jumpsuits & Rompers",resultName:"Women's Jumpsuits & Rompers"},
+{menuItem:"Shorts",resultName:"Women's Shorts"},
+{menuItem:"Skirts",resultName:"Women's Skirts"},
+{menuItem:"Swimwear & Cover Ups",resultName: "Women's Swimwear"},
+{menuItem:"Pants",resultName: "Women's Pants"},
+{menuItem:"Activewear",resultName: "Women's Athletic Clothing"},
+{menuItem:"Sleepwear", resultName: "Women's Sleepwear"},
+{menuItem:"Coats & Outerwear",resultName:  "Women's Coats & Outerwear"},
+{menuItem:"Plus Size",resultName: "Women's Plus Size Clothing"},
+{menuItem:"Work & Duty Apparel",resultName:"Women's Work & Duty Clothing"},
+{menuItem:"All Women's Accessories",resultName: "Women's Accessories"},
 {menuItem:"Handbags",resultName: "Handbags"},
 {menuItem:"Backpacks",resultName: "Women's Backpacks"},
 {menuItem:"Wallets",resultName: "Women's Wallets"},
@@ -57,33 +59,33 @@ itemsandTerms = [
 {menuItem:"Sam Edelman" ,resultName:"Women's Sam Edelman Products"}
 ]
 async openWomenMenu() {
-    await browser.url('https://www.zappos.com/')
-    await this.NavWomen.waitForClickable({timeout:2000});
-    await this.NavWomen.click();
+await browser.url('https://www.zappos.com/')
+await this.NavWomen.waitForClickable({timeout:20000});
+await this.NavWomen.click();
 
 }
-    async closewomensmenu(){
-        await this.NavWomenCloseWomensButton.click();
-    }
-    async clickMenuOptionByText(linkText) {
-        const link = await $("="+linkText);
-        await expect(link).toHaveText(linkText);
-        await link.waitForClickable({timeout:2000});    
-        await link.click();
-       
-    }
+async closewomensmenu(){
+await this.NavWomenCloseWomensButton.click();
+}
+async clickMenuOptionByText(linkText) {
+const link = await $("="+linkText);
+await expect(link).toHaveText(linkText);
+await link.waitForClickable({timeout:2000});    
+await link.click();
 
-    async validateNavigation(resultName){
-        await this.header.waitForDisplayed({timeout:1000});
-        const h1 = await this.header.getText();
-        await expect((h1.toLowerCase()).includes(resultName.toLowerCase()));
+}
 
-        const countText = await this.resultcount.getText();
+async validateNavigation(resultName){
+await this.header.waitForDisplayed({timeout:10000});
+const h1 = await this.header.getText();
+await expect((h1.toLowerCase()).includes(resultName.toLowerCase()));
 
-         expect(countText.includes("items found"));
-        
-    }
-    
+const countText = await this.resultcount.getText();
+
+expect(countText.includes("items found"));
+
+}
+
 
 
 
